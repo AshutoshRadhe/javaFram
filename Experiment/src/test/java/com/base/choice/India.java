1a913e0302ec8683aeb625c39a8a05098472937c
Ashutosh <Ashutosh@DESKTOP-6PKJUM2>
2019-05-13 12:34:22 +0530

    expriment

---
+++ Experiment/src/test/java/com/base/choice/India.java
@@ -0,0 +1,11 @@
+package com.base.choice;
+
+public class India {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("vande mataram");
+
+	}
+
+}
